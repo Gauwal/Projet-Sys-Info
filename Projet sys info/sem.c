0cd5558753b43e85e85bcc299bd55bc7ca71491b
armeirlaen <81042055+armeirlaen@users.noreply.github.com>
2021-12-05 17:43:16 +0100

    Sem

--- Projet sys info/sem.c
+++ Projet sys info/sem.c
@@ -5,120 +5,52 @@
 #include <string.h>
 #include <semaphore.h>
 
-pthread_mutex_t mutex_readcount;
-pthread_mutex_t mutex_writecount;
-
-sem_t db; 
-sem_t reader_block; 
-
-int readcount=0; 
-int writecount=0; 
-int writing_nb = 640; 
-int reading_nb = 2560; 
-
-
-void write_database(){
-}
-void read_database(){
-}
-void waste_time(){
-  while(rand() > RAND_MAX/10000);
+void my_sem_init(my_sem *sem, int pshared){
+    *sem =malloc(sizeof(my_sem));
+    sem->value= pshared;
+    sem->mod= 0;
+    sem->wait= 0;
 }
 
-void writer(void){
-  while(true){
-    waste_time();
-    pthread_mutex_lock(&mutex_writecount);
+void my_sem_destroy(my_sem *sem){
+    free(sem);
+}
 
-    if(writing_nb <= 0){
-      pthread_mutex_unlock(&mutex_writecount);
-      break;
-    }
-    else
-      writing_nb--;
-    writecount++;
-    if(writecount==1)
-      sem_wait(&reader_block);
+void my_sem_wait(my_sem *sem){
+    lock(&(sem->wait));
+    while(sem->value<=0){}
+    lock(&(sem->mod));
+    (sem->value)--;
+    unlock(&(sem->wait));
+    unlock(&(sem->mod));
+}
 
-    pthread_mutex_unlock(&mutex_writecount);
+void my_sem_post{
+    lock(&(sem->mod));
+    (sem->value)++;
+    unlock(&(sem->mod));        
+}
 
-    sem_wait(&db);
 
-    write_database();
-    sem_post(&db);
-
-    pthread_mutex_lock(&mutex_writecount);
-
-    writecount--;
-   
-    if(writecount==0)
-      sem_post(&reader_block);
-
-    pthread_mutex_unlock(&mutex_writecount);
+void lock(int *verrou){
+  while(*verrou==1){
+    asm("movl $1, %%eax;"
+    "xchgl %%eax, %1;"
+    "movl %%eax,%0;"
+        :"=r" (*verrou)
+        :"r" (*verrou)
+        :"%eax"
+    );
   }
 }
 
-void reader(void){
-  while(true){
-    sem_wait(&reader_block);
-    pthread_mutex_lock(&mutex_readcount);
-
-    if(reading_nb <= 0){
-      pthread_mutex_unlock(&mutex_readcount);
-      sem_post(&reader_block);
-      break;
-    }
-    else
-      reading_nb--;
-    readcount++;
-    if(readcount==1)
-      sem_wait(&db);
-
-    pthread_mutex_unlock(&mutex_readcount);
-    sem_post(&reader_block);
-    read_database();
-    pthread_mutex_lock(&mutex_readcount);
-
-    readcount--;
-    if(readcount==0)
-      sem_post(&db);
-
-    pthread_mutex_unlock(&mutex_readcount);
-    waste_time();
-  }
-}
-
-
-int main(int argc, char *argv[]){
-  if(argc != 3){
-    return EXIT_FAILURE;
-  }
-  int nb_writers = atoi(argv[1]);
-  int nb_readers = atoi(argv[2]);
-  int err;
-  int i;
-  
-  pthread_t writers[nb_writers];
-  pthread_t readers[nb_readers];
-
-   for(i=0; i<nb_writers; i++){
-     pthread_create(&writers[i], NULL,(void *) writer, NULL);
-   }
-   for(i=0; i<nb_readers; i++){
-     pthread_create(&readers[i], NULL, (void *)reader, NULL);
-   }
-   
-   for(i=0; i<nb_writers; i++){
-     pthread_join(writers[i], NULL);
-   }
-   for(i=0; i<nb_readers; i++){
-     pthread_join(readers[i], NULL);
-   }
-   
-   pthread_mutex_destroy(&mutex_readcount);
-   pthread_mutex_destroy(&mutex_writecount);
-   
-   sem_destroy(&reader_block);
-   sem_destroy(&db);
-   return EXIT_SUCCESS;
+void unlock(int *verrou){
+  int arg1=0;
+  asm("movl $0, %%eax;"
+    "xchgl %%eax, %1;"
+    "movl %%eax,%0;"
+        :"=r" (*verrou)
+        :"r" (*verrou)
+        :"%eax"
+    );
 }
--- Projet sys info/sem.h
+++ Projet sys info/sem.h
@@ -5,120 +5,16 @@
 #include <string.h>
 #include <semaphore.h>
 
-pthread_mutex_t mutex_readcount;
-pthread_mutex_t mutex_writecount;
 
-sem_t db; 
-sem_t reader_block; 
-
-int readcount=0; 
-int writecount=0; 
-int writing_nb = 640; 
-int reading_nb = 2560; 
-
-
-void write_database(){
-}
-void read_database(){
-}
-void waste_time(){
-  while(rand() > RAND_MAX/10000);
-}
-
-void writer(void){
-  while(true){
-    waste_time();
-    pthread_mutex_lock(&mutex_writecount);
-
-    if(writing_nb <= 0){
-      pthread_mutex_unlock(&mutex_writecount);
-      break;
-    }
-    else
-      writing_nb--;
-    writecount++;
-    if(writecount==1)
-      sem_wait(&reader_block);
-
-    pthread_mutex_unlock(&mutex_writecount);
-
-    sem_wait(&db);
-
-    write_database();
-    sem_post(&db);
-
-    pthread_mutex_lock(&mutex_writecount);
-
-    writecount--;
-   
-    if(writecount==0)
-      sem_post(&reader_block);
-
-    pthread_mutex_unlock(&mutex_writecount);
-  }
-}
-
-void reader(void){
-  while(true){
-    sem_wait(&reader_block);
-    pthread_mutex_lock(&mutex_readcount);
-
-    if(reading_nb <= 0){
-      pthread_mutex_unlock(&mutex_readcount);
-      sem_post(&reader_block);
-      break;
-    }
-    else
-      reading_nb--;
-    readcount++;
-    if(readcount==1)
-      sem_wait(&db);
-
-    pthread_mutex_unlock(&mutex_readcount);
-    sem_post(&reader_block);
-    read_database();
-    pthread_mutex_lock(&mutex_readcount);
-
-    readcount--;
-    if(readcount==0)
-      sem_post(&db);
-
-    pthread_mutex_unlock(&mutex_readcount);
-    waste_time();
-  }
+typedef struct my_sem{
+    int value;
+    int mod;
+    int wait;
 }
 
 
-int main(int argc, char *argv[]){
-  if(argc != 3){
-    return EXIT_FAILURE;
-  }
-  int nb_writers = atoi(argv[1]);
-  int nb_readers = atoi(argv[2]);
-  int err;
-  int i;
-  
-  pthread_t writers[nb_writers];
-  pthread_t readers[nb_readers];
+void my_sem_init(my_sem *sem, int pshared);
 
-   for(i=0; i<nb_writers; i++){
-     pthread_create(&writers[i], NULL,(void *) writer, NULL);
-   }
-   for(i=0; i<nb_readers; i++){
-     pthread_create(&readers[i], NULL, (void *)reader, NULL);
-   }
-   
-   for(i=0; i<nb_writers; i++){
-     pthread_join(writers[i], NULL);
-   }
-   for(i=0; i<nb_readers; i++){
-     pthread_join(readers[i], NULL);
-   }
-   
-   pthread_mutex_destroy(&mutex_readcount);
-   pthread_mutex_destroy(&mutex_writecount);
-   
-   sem_destroy(&reader_block);
-   sem_destroy(&db);
-   return EXIT_SUCCESS;
-}
+void my_sem_wait(my_sem *sem);
+
+void my_sem_post(my_sem *sem);
